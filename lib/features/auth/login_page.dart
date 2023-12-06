3424c503a2fdc061d6859c8d31629027fdfef5eb
omar <abukarakiomar@gmail.com>
2023-12-06 23:01:08 +0300

    add title

--- lib/features/auth/login_page.dart
+++ lib/features/auth/login_page.dart
@@ -6,7 +6,7 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(title: Text('data')),
     );
   }
 }
