fc55a9b4307400e12c679195f21d7bbf089e0f19
AyhamSaleh <95443899+AyhamSaleh@users.noreply.github.com>
2023-12-06 23:05:51 +0300

    add database

---
+++ lib/core/database/database.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class dataBase extends StatelessWidget {
+  const dataBase({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
